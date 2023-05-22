d9b7aa91a7a353853bb87a3715495e3747a07595
Awsam Barahmeh <awsam.haitham@outlook.com>
2023-05-22 19:55:03 +0300

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
 const Home = () => {
-    const handleCklick = () => {
-        console.log("Hello Ninnga");
+    const handleCklick = (e) => {
+        console.log("Hello Ninnga", e);
     }
 
-    const handleCklickAgain = (name) => {
-        console.log("Hello " + name);
+    const handleCklickAgain = (name,e) => {
+        console.log("Hello " + name, e.target);
     }
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
             <button onClick={handleCklick}>Click me</button>
-            <button onClick={() => handleCklickAgain("Awsam")}>Click me again</button>
+            <button onClick={(e) => handleCklickAgain("Awsam", e)}>Click me again</button>
         </div>
      );
 }
