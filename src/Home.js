ddccb3dc01ed1f8d315223c054376d13fdb1637d
Awsam Barahmeh <awsam.haitham@outlook.com>
2023-05-27 15:31:32 +0300

    Update

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
     const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
+       setTimeout(()=> {
         fetch("http://localhost:8000/blogs")
         .then(res => {
             return res.json()
@@ -15,6 +16,7 @@ const Home = () => {
             setBlogs(data);
             setIsPending(false);
         })
+       }, 1000)
     }, [])
 
     return ( 
