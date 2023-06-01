a07914821700269d86ff213d031fe3b73f76e90c
Awsam Barahmeh <awsam.haitham@outlook.com>
2023-06-01 13:42:17 +0300

    Finel-Update

--- src/NotFound.js
+++ src/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
     return ( 
         <div className="NotFound">
             <h2>Sorry</h2>
-            <p>That page cannot be found</p>
+            <p>That page cannot be found :(</p>
             <Link to="/">Back to the homepage...</Link>
         </div>
      );
